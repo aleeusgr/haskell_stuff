123d9a82ee827b7d219e584bd6940daceab6a11d
alexeusgr <alexeusgr@gmail.com>
2022-06-10 21:45:35 +0300

    fighting folds

--- Arithmetic.hs
+++ Arithmetic.hs
@@ -5,7 +5,8 @@ prime n = [x | x <- [1..n], n `mod` x == 0 ]  == [1,n]
 --32. Greatest Common Divisor
 gcd' :: Integral a => a -> a -> a
 gcd' x y = last [xs | xs <- [1..x], x `rem` xs == 0, ys <- [1..y],  y `rem` ys == 0, xs==ys]
---best 
+--
+--best, recursive
 myGCD :: Integer -> Integer -> Integer
 myGCD a b
       | b == 0     = abs a
